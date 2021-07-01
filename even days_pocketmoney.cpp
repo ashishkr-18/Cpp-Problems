c3bf7374047b8b77464fd5324eef06c3cb25e916
ashishkr-18 <67505020+ashishkr-18@users.noreply.github.com>
2021-07-01 20:00:22 +0530

    go to party on even date.cpp

---
+++ even days_pocketmoney.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int pocketmoney=3000;
+    for(int date=1; date<=30; date++){
+        if(date%2==0){
+        continue;
+        }
+        if(pocketmoney==0){
+        break;
+        }
+    }
+    cout<<"can go to party"<<endl;
+    pocketmoney=pocketmoney-300;
+}
