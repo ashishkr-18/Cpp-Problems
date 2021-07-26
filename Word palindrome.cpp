9ed7de7c1304c3e66f16f9ae463b14e7846a35b1
ashishkr-18 <67505020+ashishkr-18@users.noreply.github.com>
2021-07-26 09:18:49 +0530

    NITIN=NITIN (Palindrome)

---
+++ Word palindrome.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int n;
+   cin>>n;
+   char arr[n+1];
+   cin>>arr;
+   bool check = true;
+   for(int i=0; i<n; i++){
+      if(arr[i]!=arr[n-1-i]){
+         check=false;
+         break;
+      }
+   }
+   if(check){
+      cout<<"Word is a palindrome";
+   }else{
+      cout<<"Word is not a palindrome";
+   }
+   return 0;
+}
